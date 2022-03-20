5d1e62df18873d1dc7a813cc7547129df4dc27e4
simcha fridman <simcha1095@gmail.com>
2022-03-20 22:39:36 +0200

    lesson 01 model

---
+++ app/Models/Article.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    // שדות שמקבלים מידע
+    protected $fillable = ['title', 'article_text'];
+
+    // שדות שלא מקבלים מידע
+    protected $guarded = ['id'];
+
+    // כדי שהמודול יצביע על טבלה אחרת
+    protected $table = 'article';
+
+    // המפתח של הטבלה הוא לא id רגיל
+    protected $primaryKey = 'article_id';
+
+    public $incrementing = false;
+
+    //כמה להביא כל פעם
+    protected $perPage = 5;
+
+    // לכבות את השדות תאריך
+    public $timestamps = false;
+
+    // שינוי שדות התאריכים
+    public const CREATED_AT = 'created';
+    public const UPDATED_AT = 'updated';
+
+    // שדות תאריכים נוספים
+    protected $dates = [''];
+
+    // פורמט השמירה של התאריך 
+    protected $dateFormat = '';
+}
